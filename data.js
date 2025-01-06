c8598344ee5ed95e074fac2ceea8c6f30c379816
garusudivakar <garusudivakar@gmail.com>
2025-01-06 17:56:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+
+let movies=[
+    {
+        name:"Jhansi",
+        des:'Lorem ipsum dolor sit amet consecterur,adipisicing elit.optio accusamus quaerat harum.Lorem ipsum dolor sit amet consecterur, adipisicing elit. optio accusamus quaerat harum.',
+        image:"images/slider-1.png",
+    },
+    {
+        name:"The Warrior",
+        des:'Lorem ipsum dolor sit amet consecterur,adipisicing elit.optio accusamus quaerat harum.Lorem ipsum dolor sit amet consecterur, adipisicing elit. optio accusamus quaerat harum.',
+        image:"images/slider-2.png",
+    },
+    {
+        name:"Exposed",
+        des:'Lorem ipsum dolor sit amet consecterur,adipisicing elit.optio accusamus quaerat harum.Lorem ipsum dolor sit amet consecterur, adipisicing elit. optio accusamus quaerat harum.',
+        image:"images/slider-3.png",
+    },
+    {
+        name:"Vikram vedha",
+        des:'Lorem ipsum dolor sit amet consecterur,adipisicing elit.optio accusamus quaerat harum.Lorem ipsum dolor sit amet consecterur, adipisicing elit. optio accusamus quaerat harum.',
+        image:"images/slider-4.png",
+    },
+    {
+        name:"Bheemlanayak",
+        des:'Lorem ipsum dolor sit amet consecterur,adipisicing elit.optio accusamus quaerat harum.Lorem ipsum dolor sit amet consecterur, adipisicing elit. optio accusamus quaerat harum.',
+        image:"images/slider-5.png",
+    }
+]
